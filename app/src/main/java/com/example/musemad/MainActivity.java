f38eea1ad9a6cfb8e2d3cfa9a568dadfa0c3bbed
gabrielfonseca333 <gabrielfonsecag@gmail.com>
2024-01-08 13:28:02 +0100

    Navegar en el bottom navigation

--- app/src/main/java/com/example/musemad/MainActivity.java
+++ app/src/main/java/com/example/musemad/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.musemad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.ClipData;
 import android.os.Bundle;
 
+import com.example.musemad.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.id_home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.id_map) {
+                replaceFragment(new MapFragment());
+            } else if (itemId == R.id.id_person) {
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+
+
+    }//onCreate fin
+
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
     }
-}
+
+}//fin
