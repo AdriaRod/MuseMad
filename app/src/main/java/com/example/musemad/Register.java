d3209b461d32751f1c4799fa2821e2f9c15f2f82
AdriaRod <adriarodriguez34@gmail.com>
2024-01-08 13:55:42 +0100

    Glide + fondo blur

--- app/src/main/java/com/example/musemad/Register.java
+++ app/src/main/java/com/example/musemad/Register.java
@@ -2,7 +2,14 @@ package com.example.musemad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Glide.with(this).load(R.drawable.fondologin)
+                .transform(new BlurTransformation(25))
+                .into((ImageView) findViewById(R.id.backgroundImage));
+    }
+
+    public void openLogin(View v) {
+        Intent intent = new Intent(Register.this, Login.class);
+        startActivity(intent);
     }
 }
