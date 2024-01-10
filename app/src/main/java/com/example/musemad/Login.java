f0de525188e5b78f8c67052e43ef7eb15866376e
gabrielfonseca333 <gabrielfonsecag@gmail.com>
2024-01-10 13:51:30 +0100

    añadido a drawable icono app

--- app/src/main/java/com/example/musemad/Login.java
+++ app/src/main/java/com/example/musemad/Login.java
@@ -1,28 +1,46 @@
 package com.example.musemad;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class Login extends AppCompatActivity {
+
+    Button b_invitado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //boton invitado
+        b_invitado = findViewById(R.id.invitado);
+
         Glide.with(this).load(R.drawable.fondologin)
                 .transform(new BlurTransformation(25))
                 .into((ImageView) findViewById(R.id.backgroundImage));
-    }
+
+        b_invitado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlertDialogButtonClicked();
+            }
+        });
+
+    } //fin del onCreate
 
     public void openMain(View v) {
         Intent intent = new Intent(Login.this, MainActivity.class);
@@ -35,4 +53,35 @@ public class Login extends AppCompatActivity {
         Intent intent = new Intent(Login.this, Register.class);
         startActivity(intent);
     }
-}
+
+    public void showAlertDialogButtonClicked(){
+
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+
+        builder.setTitle("Titulo");
+        builder.setMessage("Mensaje");
+        //builder.setIcon();
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+}//fin de las class Login
