c3e89fe3628e292f611ad9858c93d4112b68bf80
AdriaRod <adriarodriguez34@gmail.com>
2024-02-12 13:51:46 +0100

    Added Settings Activity

--- app/src/main/java/com/adga/musemad/fragments/HomeFragment.java
+++ app/src/main/java/com/adga/musemad/fragments/HomeFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -17,6 +18,7 @@ import com.adga.musemad.Museum;
 import com.adga.musemad.MuseumAdapter;
 import com.adga.musemad.MuseumDetail;
 import com.adga.musemad.R;
+import com.adga.musemad.Settings;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +32,15 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageView imageButton = view.findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Settings.class);
+                startActivity(intent);
+            }
+        });
 
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
