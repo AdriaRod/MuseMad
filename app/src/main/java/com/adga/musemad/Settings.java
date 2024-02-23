a295b79de4ec629f7fa9e6a3ca25fa858143ccd4
AdriaRod <adriarodriguez34@gmail.com>
2024-02-23 21:19:39 +0100

    New functions to Settings

--- app/src/main/java/com/adga/musemad/Settings.java
+++ app/src/main/java/com/adga/musemad/Settings.java
@@ -1,14 +1,94 @@
 package com.adga.musemad;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
+    private RelativeLayout baboutus, bcontactus, blogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        baboutus = findViewById(R.id.aboutus);
+        bcontactus = findViewById(R.id.contactus);
+        blogout = findViewById(R.id.cerrarsesion);
+
+        blogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDialogoConfirmacionCerrarSesion();
+            }
+        });
+
+        bcontactus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviarCorreo();
+            }
+        });
+
+        baboutus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.this, AboutUs.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    private void mostrarDialogoConfirmacionCerrarSesion() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmar Cierre de Sesión");
+        builder.setMessage("¿Seguro que quieres cerrar sesión?");
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                cerrarSesion();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void cerrarSesion() {
+        Intent intent = new Intent(Settings.this, Login.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish(); // Finaliza la actividad actual para que el usuario no pueda volver atrás con el botón de retroceso
+    }
+
+
+    private void enviarCorreo() {
+        String[] direccionesCorreo = {"musemadapp@gmail.com"};
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+        intent.putExtra(Intent.EXTRA_EMAIL, direccionesCorreo);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(Intent.createChooser(intent, "Elige una aplicación de correo electrónico"));
+        } else {
+            // Muestra un mensaje si no hay ninguna aplicación de correo electrónico instalada
+            Toast.makeText(this, "No hay aplicaciones de correo electrónico instaladas.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
 }
